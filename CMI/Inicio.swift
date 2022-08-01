5efcf83975e481bc61c97ba6c1db370c045aec5c
Invitado <invitado@MacBook-Pro-de-Invitado.local>
2022-08-01 14:15:39 -0500

    Se modifica vista de módulos

--- CMI/Inicio.swift
+++ CMI/Inicio.swift
@@ -28,10 +28,17 @@ struct Inicio: View {
                         .font(.body)
                         .multilineTextAlignment(.leading)
                 }.padding([.horizontal,.vertical])
-                ModuloView()
+                ScrollView(.horizontal, showsIndicators: true){
+                    HStack(alignment: .center, spacing: 10){
+                        Modulo(imagen: "Image_Modulo_I", titulo: "Módulo I", texto: "Indicadores de la contribución de las Instituciones de los subsistemas de la DGESUI al Programa Sectorial de Educación 2020-2024", listaIndicadores: listaModulo_I)
+                        Modulo(imagen: "Image_Modulo_II", titulo: "Módulo II", texto: "Indicadores de las Matrices de Indicadores para Resultados (MIR) de los programas presupuestales que opera la DGESUI", listaIndicadores: listaModulo_II)
+                        Modulo(imagen: "Image_Modulo_III", titulo: "Módulo III", texto: "Repositorio de Datos Estadísticos (RDE) de la DGESUI", listaIndicadores: listaModulo_III)
+                    }.padding()
+                }
+                
             }.foregroundColor(Color("gris_2"))
                 .navigationBarHidden(true)
-        }
+        }.padding(.bottom)
     }
 }
 
--- CMI/Modulo.swift
+++ CMI/Modulo.swift
@@ -1,55 +1,44 @@
 import SwiftUI
 
-struct ModuloView: View {
-    
-    var body: some View {
-            HStack(alignment: .center, spacing: 10){
-                Modulo(imagen: "Image_Modulo_I", titulo: "Módulo I", texto: "Indicadores de la contribución de las Instituciones de los subsistemas de la DGESUI al Programa Sectorial de Educación 2020-2024", listaIndicadores: listaModulo_I)
-                Modulo(imagen: "Image_Modulo_II", titulo: "Módulo II", texto: "Indicadores de las Matrices de Indicadores para Resultados (MIR) de los programas presupuestales que opera la DGESUI", listaIndicadores: listaModulo_II)
-            }.padding(.top)
-            HStack{
-                Modulo(imagen: "Image_Modulo_III", titulo: "Módulo III", texto: "Repositorio de Datos Estadísticos (RDE) de la DGESUI", listaIndicadores: listaModulo_III)
-            }.padding(.bottom)
-    }
-    
-    
-}
-
 struct Modulo: View {
     var imagen: String
     var titulo: String
     var texto: String
     var listaIndicadores: [Indicadores]
     var body: some View{
-        VStack(spacing: 15){
+        VStack(spacing: 10){
             Image(imagen)
                 .resizable()
-                .frame(width: 80, height: 80)
+                .frame(width: 100, height: 100)
                 .foregroundColor(Color("dorado"))
+                .padding()
+            
             
             Text(.init(titulo))
                 .font(.title3.bold())
                 .foregroundColor(Color("gris_2"))
-            
+                    
+            Spacer()
             Text(.init(texto))
                 .multilineTextAlignment(.center)
                 .font(.body)
                 .foregroundColor(Color("gris_2"))
-            
+                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+            Spacer()
             NavigationLink(destination: DetalleModulo(modulo: titulo, titulo: texto, listaIndicadores: listaIndicadores)) {
                 Text("Ver")
                     .font(.headline)
-                    .frame(width: 80, height: 40)
+                    .frame(width: 100, height: 40)
                     .foregroundColor(Color("rosita"))
                     .overlay(
                         RoundedRectangle(cornerRadius: 3)
                             .stroke(Color("rosita"), lineWidth: 2)
-                        )
-            }
+                    )
+            }.padding()
         }.padding(.horizontal)
-        .frame(width: 200, height: 400)
+            .frame(width: 250, height: 450)
             .overlay(
-                RoundedRectangle(cornerRadius: 40)
+                RoundedRectangle(cornerRadius: 20)
                     .stroke(Color("dorado"), lineWidth: 5)
             )
     }
@@ -57,6 +46,6 @@ struct Modulo: View {
 
 struct ModuloView_Previews: PreviewProvider {
     static var previews: some View {
-            ModuloView()
+        Modulo(imagen: "Image_Modulo_II", titulo: "Modulo II", texto: "Indicadores de las Matrices de Indicadores para Resultados (MIR) de los programas presupuestales que opera la DGESUI", listaIndicadores: listaModulo_II)
     }
 }
